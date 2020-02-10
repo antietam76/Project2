9a64a1ab8b9257d97571bd4f705bbccbfc1ff51b
antietam76 <54750256+antietam76@users.noreply.github.com>
2020-02-10 16:49:09 -0500

    Add files via upload
    2/10/20

--- camera_verification.py
+++ camera_verification.py
@@ -9,77 +9,51 @@ Created on Fri Jan 31 11:55:31 2020
 
 import numpy as np
 import cv2
-
+import matplotlib.pyplot as plt
+#import sympy as sym
 
 cap = cv2.VideoCapture(0)
 
 Fx = 1013.4176 #pixels
 Fy = 1011.2264 #pixels
-Or = 659.5295 #pixels
-Oc = 355.5793 #pixels
-r = 650     #pixels
-c = 540     # pixels
-z = 747.54 #pixels ### #1219.2 * .61314#mm to pixels 
+#Or = 659.5295 #pixels
+#Oc = 355.5793 #pixels
+#r = 650     #pixels
+#c = 540     # pixels
+#z = 747.54 #pixels ### #1219.2 * .61314#mm to pixels 
+R = np.array([[-0.9995, -0.0135, -0.0293],
+               [0.0110, -0.9966,  0.0812],
+              [-0.0303,  0.0809,  0.9963],
+              [96.1631,  330.2148, 1359.4]])
+K = np.array([[1385.5868,    0,         0],
+              [0,        1377.2714,     0],
+              [648.061,    264.0641,     1]])
+Z = [-150, -70, -150, -80, 0]
+
+#x = sym.Symbol('x')
+#y = sym.Symbol('y')
 
 
-def capture_img():
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if ret==True:
-            cv2.imwrite('rec.png',frame)
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-           
-                break
-        else:
-            break
+    
+
+ret, frame = cap.read()
 
 
-def calculate_x_y(Or, Oc, r, c, Fx, Fy, z):
-    x = ((Or - r)/Fx)*z
-    y = ((Oc - c)/Fy)*z
-    x = x/0.61314
-    y = y/0.61314
-    print (x, y ) 
-    return (x,y)  
-  
-#capture_img()
+plt.figure(0)
+plt.clf()
+plt.imshow(frame)
+            
+source = np.asarray(plt.ginput(1),np.float32)
+#            if cv2.waitKey(1) & 0xFF == ord('q'):
+#     
+r = source[0,0]
+c = source [0,1]   
+i_f =np.array([[r, c, 1]])   
 
-
-
-calculate_x_y(Or, Oc, r, c, Fx, Fy, z)
-
-
-
+ 
+for z in Z:
+    C= np.array([[1, 1, z, 1]])
+    aall = C.dot(R.dot(K))
+    wow = np.linalg.solve(aall,np.transpose(i_f))
+ 
 cap.release()
-cv2.destroyAllWindows()
-## Load image
-#im = cv2.imread('rec.png')
-#
-## Convert to grayscale and threshold
-#imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-#ret,thresh = cv2.threshold(imgray,1,255,0)
-#
-## Find contours, draw on image and save
-#if cv2.getVersionMajor() in [2, 4]:
-#    # OpenCV 2, OpenCV 4 case
-#    contours, hier = cv2.findContours(
-#                    thresh.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-#else:
-#    # OpenCV 3 case
-#    image, contours, hier = cv2.findContours(
-#                    thresh.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-#
-##imgray, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-#cv2.drawContours(im, contours, -1, (0,255,0), 3)
-#cv2.imwrite('result.png',im)
-#
-## Show user what we found
-#for cnt in contours:
-#   (x,y),radius = cv2.minEnclosingCircle(cnt)
-#   center = (int(x),int(y))
-#   radius = int(radius)
-#   print('Contour: centre {},{}, radius {}'.format(x,y,radius))
-#   
-#cap.release()
-#cv2.destroyAllWindows()
