f7cecaec0d55c258525567d80b42e9839fa69f11
antietam76 <54750256+antietam76@users.noreply.github.com>
2020-02-10 22:14:53 -0500

    Add files via upload

--- camera_verification.py
+++ camera_verification.py
@@ -11,10 +11,9 @@ Created on Fri Jan 31 11:55:31 2020
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-#import sympy as sym
+
 image = ["/home/jacobksu/Pictures/1.jpg","/home/jacobksu/Pictures/2.jpg","/home/jacobksu/Pictures/3.jpg","/home/jacobksu/Pictures/4.jpg","/home/jacobksu/Pictures/5.jpg"]
 for i in image:
-#cap = cv2.VideoCapture(0)
     g = cv2.imread(i)
     
     Fx = 1385.5868 #pixels
@@ -22,28 +21,17 @@ for i in image:
     Or = 648.061 #pixels
     Oc = 264.0641 #pixels
     z = 1260 
-    H_matrix = np.array([[-0.9995, -0.0135, -0.0293],
-                   [0.0110, -0.9966,  0.0812],
-                  [-0.0303,  0.0809,  0.9963],
-                  [96.1631,  330.2148, 1359.4]])
-    #K = np.array([[1385.5868,    0,         0],
-    #              [0,        1377.2714,     0],
-    #              [648.061,    264.0641,     1]])
-    #Z = [-150, -70, -150, -80, 0]
-    
-    #x = sym.Symbol('x')
-    #y = sym.Symbol('y')
-    
-    #ret, frame = cap.read()
-    
-    
+    H_matrix = np.array([[-0.9995, -0.0135, -0.0293, 96.1631 ],
+                         [0.0110,  -0.9966,  0.0812, 330.2148],
+                         [-0.0303,  0.0809,  0.9963, 1359.4  ],
+                         [0,        0,       0,      1    ]])
+        
     plt.figure(0)
     plt.clf()
     plt.imshow(g)
                 
     source = np.asarray(plt.ginput(1),np.float32)
-    #            if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     
+    
     r = source[0,0]
     c = source [0,1]   
     i_f =np.array([[r, c, 1]])   
@@ -62,3 +50,12 @@ for i in image:
     
      
     plt.clf()
+#K = np.array([[1385.5868,    0,         0],
+    #              [0,        1377.2714,     0],
+    #              [648.061,    264.0641,     1]])
+    #Z = [-150, -70, -150, -80, 0]
+    
+    #x = sym.Symbol('x')
+    #y = sym.Symbol('y')
+    
+    #ret, frame = cap.read()
